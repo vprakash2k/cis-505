f7f6ab78372d3394bfe70e3e00e0b374a6e2d3e0
prakash <pviduthalairaju@my365.bellevue.edu>
2025-03-18 00:40:19 -0500

    Added PrakashViduthalairajuHelloWorld

---
+++ Module_1/HelloWorldApp/PrakashViduthalairajuHelloWorld.java
@@ -0,0 +1,10 @@
+public class PrakashViduthalairajuHelloWorld {
+
+    public static void main(String[] args) {
+
+        String firstName = "YourFirstName";
+        String lastName = "YourLastName";
+
+        System.out.printf("Hello World from %s %s.\n", firstName, lastName);
+    }
+}
