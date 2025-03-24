10a61cbbaade02081621423cdc02b6d8f410765f
prakash <pviduthalairaju@my365.bellevue.edu>
2025-03-23 22:45:11 -0500

    Add works in Module2 - creating .jar file

---
+++ Module_2/JarPracticeFiles/ViduthalairajuJarTwo.java
@@ -0,0 +1,8 @@
+/*
+ * Liang, Y.D. (2019). Introduction to Java Programming and Data Structures: Comprehensive Version (12th ed.). Pearson Education, Inc.
+ * Modifications by R. Krasso, 2021
+ * Viduthalairaju, Prakash. (2025). CIS 505 Intermediate Java Programming. Bellevue University.
+ */
+
+public class ViduthalairajuJarTwo {
+}
