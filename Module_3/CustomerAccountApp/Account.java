863982b5ef88b669220f52f113be909464126a73
prakash <pviduthalairaju@my365.bellevue.edu>
2025-03-30 21:52:26 -0500

    Assignment for module3

---
+++ Module_3/CustomerAccountApp/Account.java
@@ -0,0 +1,39 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Account {
+    // Private balance field
+    private double balance = 200.00;
+
+    // Getter for balance
+    public double getBalance() {
+        return balance;
+    }
+
+    // Deposit method
+    public void deposit(double amt) {
+        if (amt > 0) {
+            balance += amt;
+        }
+    }
+
+    // Withdraw method
+    public void withdraw(double amt) {
+        if (amt <= balance && amt > 0) {
+            balance -= amt;
+        }
+    }
+
+    // Display menu method
+    public void displayMenu() {
+        System.out.println("1. Enter <D/d> for Deposit");
+        System.out.println("2. Enter <W/w> for Withdraw");
+        System.out.println("3. Enter <B/b> for Exit");
+    }
+
+    // Get transaction date
+    public String getTransactionDate() {
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+        return sdf.format(new Date());
+    }
+}
---
+++ Module_3/CustomerAccountApp/CustomerDB.java
@@ -0,0 +1,13 @@
+public class CustomerDB {
+    public static Customer getCustomer(int id) {
+        if (id == 1007) {
+            return new Customer("Prakash V", "763 Widgeon Dr", "Wheeling", "60036");
+        } else if (id == 1008) {
+            return new Customer("Jennifer Smith", "8422 Grover Ave,", "Bellevue", "68123");
+        } else if (id == 1009) {
+            return new Customer("Alice Johnson", "2000 LakeCook Rd", "Riverwoods", "600123");
+        } else {
+            return new Customer(); // Return a default customer if no match
+        }
+    }
+}
